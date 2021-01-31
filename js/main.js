4e40ac8a7094a1667c9b07b0a3ca5c63e019c720
bahek4315 <bahek4315@yandex.ru>
2021-01-31 21:05:50 +0300

    Сделал генератор v2, с правками

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // https://learn.javascript.ru/number
 
-function generateNumber (min, max, n) {
+function generateNumber (min, max, digits) {
   if (min < 0) {
-    return ('Min must be not less than 0');
+    alert('Min must be not less than 0');
   }
   if (max < min) {
-    return ('Max must be more than min');
+    alert('Max must be more than min');
   }
-  return ((Math.random() * (max - min)) + min).toFixed(n);
+  return ((Math.random() * (max - min)) + min).toFixed(digits);
 }
 
 generateNumber (1.1, 1.2, 5)
