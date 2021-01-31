381b75006ffa37725ace6d6d6f206e93b2881c37
bahek4315 <bahek4315@yandex.ru>
2021-01-31 19:35:43 +0300

    Сделан генератор v1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// https://learn.javascript.ru/number
+
+function generateNumber (min, max, n) {
+  if (min < 0) {
+    return ('Min must be not less than 0');
+  }
+  if (max < min) {
+    return ('Max must be more than min');
+  }
+  return ((Math.random() * (max - min)) + min).toFixed(n);
+}
