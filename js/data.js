c5e83e84c89096f45faf823ce27fe9dc45db0b93
bahek4315 <bahek4315@yandex.ru>
2021-02-10 23:24:40 +0300

    Исправлены {} скобки и сделано разбиение на модули v1

---
+++ js/data.js
@@ -0,0 +1,29 @@
+const TYPE_VALUES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const CHECK_VALUES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURE_VALUES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTO_VALUES = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+export {TYPE_VALUES, CHECK_VALUES, FEATURE_VALUES, PHOTO_VALUES};
