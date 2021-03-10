aa53a1db3b46cf6157e7fa53902c0ce500abd26f
bahek4315 <bahek4315@yandex.ru>
2021-03-10 19:46:37 +0300

    переделано дз 5.2

--- js/card-processing.js
+++ js/card-processing.js
@@ -2,21 +2,26 @@ export const setMinSum = () => {
   let buildingType = document.querySelector('#type');
   let priceInput = document.querySelector('#price');
   //без этого не работает валидация, если ничего не выбирать в теге <select>, те оставить "квартира" по дефолту, решил сделать такой код
-  if (buildingType.selectedIndex === 1) {
+  if (buildingType.value === 'flat') {
     priceInput.min = 1000;
+    priceInput.placeholder = '>1000'
   }
   buildingType.addEventListener('change', function () {
-    if (buildingType.selectedIndex === 0) {
+    if (buildingType.value === 'bungalow') {
       priceInput.min = 0;
+      priceInput.placeholder = '>0'
     }
-    if (buildingType.selectedIndex === 1) {
+    if (buildingType.value === 'flat') {
       priceInput.min = 1000
+      priceInput.placeholder = '>1000'
     }
-    if (buildingType.selectedIndex === 2) {
+    if (buildingType.value === 'house') {
       priceInput.min = 5000;
+      priceInput.placeholder = '>5000'
     }
-    if (buildingType.selectedIndex === 3) {
+    if (buildingType.value === 'palace') {
       priceInput.min = 10000;
+      priceInput.placeholder = '>10000'
     }
   })
 
@@ -26,26 +31,7 @@ export const timeSync = () => {
   let timeIn = document.querySelector('#timein');
   let timeOut = document.querySelector('#timeout');
   timeIn.addEventListener('change', function (){
-    if (timeIn.selectedIndex === 0) {
-      timeOut.selectedIndex = 0
-    }
-    if (timeIn.selectedIndex === 1) {
-      timeOut.selectedIndex = 1
-    }
-    if (timeIn.selectedIndex === 2) {
-      timeOut.selectedIndex = 2
-    }
-  })
-
-  timeOut.addEventListener('change', function (){
-    if (timeOut.selectedIndex === 0) {
-      timeIn.selectedIndex = 0
-    }
-    if (timeOut.selectedIndex === 1) {
-      timeIn.selectedIndex = 1
-    }
-    if (timeOut.selectedIndex === 2) {
-      timeIn.selectedIndex = 2
-    }
+    timeOut.value = timeIn.value;
+    timeIn.value = timeOut.value;
   })
 }
