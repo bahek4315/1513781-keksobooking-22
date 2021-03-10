4b5dde51d805cd6f90961dfe026f26516f362130
bahek4315 <bahek4315@yandex.ru>
2021-03-10 18:21:48 +0300

    просто сделал карту

---
+++ js/lock.js
@@ -0,0 +1,39 @@
+export const disablePage = () => {
+  let mainForm = document.querySelector('.ad-form');
+  let allMainFormFieldsets = mainForm.querySelectorAll('fieldset');
+  let mapForm = document.querySelector('.map__filters');
+  let allMapFormSelectors = mapForm.querySelectorAll('select');
+
+  mainForm.classList.add('ad-form--disabled');
+  mapForm.classList.add('map__filters--disabled');
+
+  for (let i = 0; i < allMainFormFieldsets.length; i++) {
+    allMainFormFieldsets[i].disabled = true;
+  }
+
+  mapForm.querySelector('fieldset').disabled = true;
+
+  for (let i = 0; i < allMapFormSelectors.length; i++) {
+    allMapFormSelectors[i].disabled = true;
+  }
+}
+
+export const enablePage = () => {
+  let mainForm = document.querySelector('.ad-form');
+  let allMainFormFieldsets = mainForm.querySelectorAll('fieldset');
+  let mapForm = document.querySelector('.map__filters');
+  let allMapFormSelectors = mapForm.querySelectorAll('select');
+
+  mainForm.classList.remove('ad-form--disabled');
+  mapForm.classList.remove('map__filters--disabled');
+
+  for (let i = 0; i < allMainFormFieldsets.length; i++) {
+    allMainFormFieldsets[i].disabled = false;
+  }
+
+  mapForm.querySelector('fieldset').disabled = false;
+
+  for (let i = 0; i < allMapFormSelectors.length; i++) {
+    allMapFormSelectors[i].disabled = false;
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // https://learn.javascript.ru/number
 import {createTenOffers} from './data.js';
-import{createCard} from './card.js';
+import {createCard} from './card.js';
+import {disablePage, enablePage} from './lock.js'
 
 let offer = createTenOffers()[0];
 
 let insertUnit = document.querySelector('#map-canvas');
 
-insertUnit.appendChild(createCard(offer));
+disablePage();
+
+/* global L:readonly */
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    console.log('Карта инициализирована')
+    enablePage()
+  })
+  .setView({
+    lat: 35.681700,
+    lng: 139.753882,
+  }, 15);
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
